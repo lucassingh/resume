ee3e06ab1e4c1d6a9ec1f38f9e9d33dd885ee101
lucassingh <lucas.singh10@gmail.com>
2019-03-16 00:06:51 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     setTimeout(function(){
         $("#content").fadeIn(2000);
         $("#img").fadeIn(2000);
+        $("#cont").fadeIn(2000);
      
      }, 3000);
 });
